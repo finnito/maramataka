ad6f146d083a1e6a4be78507fab90e8de36bb783
Finn Le Sueur <finn@lesueur.nz>
2022-04-18 17:15:01 +1200

    Adds deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /srv/maramataka.lesueur.nz/
+
+sudo git pull origin main
+
+# Do install things and make sure
+# the cache doesn't get in the way.
+sudo -u www-data composer install --profile
+sudo -u www-data php artisan migrate --path=vendor/anomaly/streams-platform/migrations/application
+sudo -u www-data php artisan migrate --all-addons
+sudo -u www-data php artisan assets:clear
+sudo -u www-data php artisan view:clear
+sudo -u www-data php artisan httpcache:clear
+
+# Make sure permissions are all good
+sudo chown -R www-data:www-data ./
+sudo chmod -R ug+rwx storage bootstrap/cache;
+sudo -u www-data composer dump-autoload --profile
